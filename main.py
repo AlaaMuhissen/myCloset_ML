1b03a85e05dc1f8f85a141e4497d6f2771d6ebb2
Alaa Muhissen <alaa.muhissen1912@gmail.com>
2024-06-18 14:50:34 +0300

    Updated the endpoint to extract  from the request body and process it

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import logging
-from fastapi import FastAPI, HTTPException, Form
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 import os
 from app.api.recognize_clothe_and_color import process_and_annotate_image
 
@@ -16,22 +17,26 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Define a Pydantic model for the request body
+class ImageRequest(BaseModel):
+    image_url: str
+
 @app.post("/recognize-clothes-and-colors/")
-async def recognize_clothes_and_colors(image_url: str = Form(...)):
+async def recognize_clothes_and_colors(request: ImageRequest):
     try:
+        # Extract image_url from the request body
+        image_url = request.image_url
         # Here you call your image processing function
         result = process_and_annotate_image(image_url)
         return JSONResponse(content=result)
     except Exception as e:
         logging.error(f"Error processing image: {e}")
         raise HTTPException(status_code=500, detail=f"Error processing image: {str(e)}")
-    
 
 @app.get("/")
-def sayHello():
+def say_hello():
     logging.info("Hello world")
     return {"message": "Hello world"}
-
 
 if __name__ == "__main__":
     import uvicorn
